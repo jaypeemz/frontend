3348d8036f2b86f3919039da563059aee472d250
taccumei <80431063+taccumei@users.noreply.github.com>
2022-04-09 01:45:03 -0700

    Add show book list UI and implement display book list

--- src/components/audiobooksList.vue
+++ src/components/audiobooksList.vue
@@ -1,37 +1,143 @@
-// <template>
-//     <h1>Audiobooks</h1>
-//     <ul v-for="(audiobook,index) in audiobooks" :key="index">
-//         <li>{{audiobook.title}}</li>
-//         <li>{{audiobook.categories}}</li>
-//     </ul>
+<template>
+  <div class="list row">
+    <div class="col-md-6">
+      <h4>Show All Books</h4>
+      <ul class="list-group">
+        <li
+          class="list-group-item"
+          :class="{ active: index == currentIndex }"
+          v-for="(book, index) in books"
+          :key="index"
+          @click="setActiveBook(book, index)"
+        >
+          {{ book.title }}
+        </li>
+      </ul>
+    </div>
 
-// </template>
+    <div class="col-md-6">
+      <div v-if="currentBook">
+        <h4>Book</h4>
+        <div>
+          <label>
+            <strong>Book Title:</strong>
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="song_title"
+            v-model="currentBook.title"
+          />
+        </div>
+        <div>
+          <label>
+            <strong>Author:</strong>
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="book_author"
+            v-model="currentBook.author"
+          />
+          <!-- {{ currentSong.author }} -->
+        </div>
+        <div>
+          <label>
+            <strong>Categories:</strong>
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="book_genre"
+            v-model="currentBook.categories"
+          />
+        </div>
+        <button class="m-3 btn btn-sm btn-danger" @click="deleteBook">
+          Delete
+        </button>
+        <button class="m-3 btn btn-sm btn-danger" @click="saveBook">
+          Update
+        </button>
+      </div>
+      <div v-else>
+        <br />
+        <p v-if="books">Please click on a Book...</p>
+      </div>
+    </div>
+  </div>
+</template>
 
-// <script>
-// import AudiobookDataService from '../../services/AudiobookDataService.js'
+<script>
+import AudiobookDataServices from '../services/AudiobookDataService';
 
-// export default {
-//     name: 'audiobook-list',
-//     data() {
-//         return {
-//             audiobooks: []
-//         }
-//     },
-//     methods: {
-//         retrieveAudiobooks() {
-//             AudiobookDataService.getAll()
-//                 .then(response => {
-//                     this.audiobooks = response.data
-//                     console.log(response.data)
-//                 })
-//                 .catch(e => {
-//                     alert(e)
-//                 })
-//         }
-//     },
-//     mounted() {
-//         this.retrieveAudiobooks()
-//     }
-// }
+export default {
+  name: "book-list",
+  data() {
+    return {
+      books: [],
+      currentBook: null,
+      currentIndex: -1,
+    };
+  },
 
-// </script>
+  methods: {
+    saveBook() {
+      AudiobookDataServices.save(this.currentBook)
+        .then((response) => {
+          // this.refreshList();
+          console.log(response);
+        })
+        .catch((e) => {
+          console.log(e);
+        })
+    },
+    deleteBook() {
+      AudiobookDataServices.delete(this.currentBook.id)
+        .then((response) => {
+          this.refreshList();
+          console.log(response);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    setActiveBook(book, index) {
+      this.currentBook = book;
+      this.currentIndex = index;
+    },
+    refreshList() {
+      this.currentBook = null;
+      this.currentIndex = -1;
+      AudiobookDataServices.get("")
+        .then((response) => {
+          this.songs = response.data;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+
+  mounted() {
+    AudiobookDataServices.get("")
+      .then(response => {
+            this.books = response.data;
+        })
+          .catch(function (error) {
+             console.log(error);
+        });
+
+        
+
+      
+  },
+};
+</script>
+
+<style>
+.list {
+  text-align: left;
+  max-width: 750px;
+  margin: auto;
+}
+</style>
--- src/services/UserDataService.js
+++ src/services/UserDataService.js
@@ -1,16 +1,10 @@
 import { def } from "@vue/shared";
 import http from "../http-common.js";
 
-class CourseDataService {
+class UserDataService {
     get(id) {
-        return http.get(`/courses/${id}`);
-    }
-    delete(id) {
-        return http.delete(`/courses/${id}`)
-    }
-    save(id) {
-        return http.put(`/courses/${id}`)
+        return http.get(`/user/${id}`);
     }
 }
 
-export default new CourseDataService();
+export default new UserDataService();
